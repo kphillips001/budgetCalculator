54672a35dfe1e22973d0bc8317aed0dbb24af332
Kevin <knphillips01@gmail.com>
2019-09-12 18:59:21 -0500

    setup app component return

--- src/App.js
+++ src/App.js
@@ -13,13 +13,22 @@ const initialExpenses = [
 
 function App() {
 const [expenses, setExpenses] = useState(initialExpenses);
-console.log(expenses);
-
+ 
   return (
     <div>
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses}/>
+      </main>
+      <h1>
+        total spending : <span className='total'>
+          ${expenses.reduce((acc, curr)=>{
+            return (acc += curr.amount); 
+            }, 0)}
+        </span>
+      </h1>
     </div>
 
   );
