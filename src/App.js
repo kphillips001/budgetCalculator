e20b00b34850f000628fb73df3335ea259ff30a2
Kevin <knphillips01@gmail.com>
2019-09-13 00:38:30 -0500

    added functionality to handle submit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseList from './components/expenselist';
 import ExpenseForm from './components/expenseform';
 import Alert from './components/alert';
-import uuid from "uuid";
+import uuid from 'uuid';
 
 const initialExpenses = [
-  {id: uuid(), charge:'rent', amount:1600},
-  {id: uuid(), charge:'car payment', amount:400},
-  {id: uuid(), charge:'credit card bill', amount:1200}
+  { id: uuid(), charge: 'rent', amount: 1600 },
+  { id: uuid(), charge: 'car payment', amount: 400 },
+  { id: uuid(), charge: 'credit card bill', amount: 1200 }
 ];
-
 function App() {
-  //*************** state values ***********
+  // ************* state values *******************
   // all expenses, add expense
-const [expenses, setExpenses] = useState(initialExpenses);
- //single expense
-const [charge, setCharge] = useState('');
-//single amount
-const [amount, setAmount] = useState('');
-//***************** functionality ***********
-
-const handleCharge = evt => {
-  setCharge(evt.target.value)
-}
-
-const handleAmount = evt => {
-  setAmount(evt.target.value)
-}
-
-const handleSubmit = evt => {
-  evt.preventDefault();
-}
-
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // ************* functionality *******************
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  const handleAmount = e => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
+    } else {
+      // handle alert called
+    }
+  };
   return (
-    <div>
+    <>
       <Alert />
       <h1>budget calculator</h1>
-      <main className="App">
-        <ExpenseForm 
-          charge={charge} 
+      <main className='App'>
+        <ExpenseForm
+          charge={charge}
           amount={amount}
           handleAmount={handleAmount}
           handleCharge={handleCharge}
           handleSubmit={handleSubmit}
-          />
-        <ExpenseList expenses={expenses}/>
+        />
+        <ExpenseList expenses={expenses} />
       </main>
       <h1>
-        total spending : <span className='total'>
-          ${expenses.reduce((acc, curr)=>{
-            return (acc += curr.amount); 
-            }, 0)}
+        total spending :{''}
+        <span className='total'>
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += parseInt(curr.amount));
+          }, 0)}
         </span>
       </h1>
-    </div>
-
+    </>
   );
 }
 
