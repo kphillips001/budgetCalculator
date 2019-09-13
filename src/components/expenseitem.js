c2ad61cee650c2130beb94ec6afc05a1217caabf
Kevin <knphillips01@gmail.com>
2019-09-12 22:30:18 -0500

    built out expenseitem component

--- src/components/expenseitem.js
+++ src/components/expenseitem.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { MdEdit, MdDelete } from 'react-icons/md';
 
-const Expenseitem = () => {
-  return (
-    <div>
-      test from Expenseitem
+const Expenseitem = ({expense}) => {
+const { id, charge, amount } = expense
+  return( 
+  <li className='item'>
+    <div className='info'>
+      <span className='expense'>{charge}</span>
+      <span className='amount'>{amount}</span>
     </div>
-  )
-}
+    <div>
+      <button className='edit-btn' aria-label='edit-button'>
+        <MdEdit />
+      </button>
+      <button className='clear-btn' aria-label='delete-button'>
+        <MdDelete />
+      </button>
+    </div>
+  </li> 
+  );
+};
 
 export default Expenseitem
