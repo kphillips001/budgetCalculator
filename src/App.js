9520d3c583aabc857fce896160a823f364df343d
Kevin <knphillips01@gmail.com>
2019-09-13 13:56:16 -0500

    built out alert component and integrated into app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ExpenseList from './components/expenselist';
 import ExpenseForm from './components/expenseform';
 import Alert from './components/alert';
 import uuid from 'uuid';
-
 const initialExpenses = [
   { id: uuid(), charge: 'rent', amount: 1600 },
   { id: uuid(), charge: 'car payment', amount: 400 },
@@ -15,29 +14,51 @@ function App() {
   // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
   // single expense
-  const [charge, setCharge] = useState("");
+  const [charge, setCharge] = useState('');
   // single amount
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState('');
+  // alert
+  const [alert, setAlert] = useState({ show: false });
   // ************* functionality *******************
+  // handle charge
+
   const handleCharge = e => {
     setCharge(e.target.value);
   };
+  // handle amount
+
   const handleAmount = e => {
     setAmount(e.target.value);
   };
+  // handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 3000);
+  };
+
+  // handle submit
+
   const handleSubmit = e => {
     e.preventDefault();
     if (charge !== '' && amount > 0) {
       const singleExpense = { id: uuid(), charge, amount };
       setExpenses([...expenses, singleExpense]);
+      handleAlert({ type: 'success', text: 'item added' });
       setCharge('');
       setAmount('');
     } else {
       // handle alert called
+      handleAlert({
+        type: 'danger',
+        text: `charge can't be empty value and amount value has to be bigger than zero`
+      });
     }
   };
   return (
     <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
       <Alert />
       <h1>budget calculator</h1>
       <main className='App'>
@@ -51,7 +72,7 @@ function App() {
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
-        total spending :{''}
+        total spending :{" "}
         <span className='total'>
           $
           {expenses.reduce((acc, curr) => {
--- src/components/alert.js
+++ src/components/alert.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
-const Alert = () => {
-  return (
-    <div>
-      testing from alert
-    </div>
-  )
-}
+const Alert = ({ type, text }) => {
+  return <div className={`alert alert-${type}`}>{text}</div>;
+};
 
-export default Alert
+export default Alert;
